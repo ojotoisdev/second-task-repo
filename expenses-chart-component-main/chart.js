448973fcc070e24815231473a8d26af0f5cb89fa
ojotoisdev <129745992+ojotoisdev@users.noreply.github.com>
2025-01-09 13:35:41 +0100

    initial commit

---
+++ expenses-chart-component-main/chart.js
@@ -0,0 +1,15 @@
+function comparisonSum(a, b) {
+    let scoreA = 0;
+    let scoreB = 0;
+
+    for(let i = 0; i < a.length; i++){
+        if(a[i] > b[i]){
+            scoreA ++;
+        }else if(a[i] < b[i]){
+            scoreB ++;
+        }
+        
+    }
+    return [scoreA, scoreB];
+    
+}
